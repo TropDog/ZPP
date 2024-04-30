e678bbcb7be850205bc82e352a1e700709c214fb
Mateusz Pustelnik <mateusz.pustelnik13@gmail.com>
2024-04-30 14:54:35 +0200

    post endpoint

---
+++ logic.py
@@ -0,0 +1,57 @@
+import cv2
+import numpy as np
+from urllib.request import Request, urlopen
+from fastapi import File, UploadFile
+import shutil, os
+
+#Function with hog algorithm
+def count_people_on_img(file_name:str) -> int:
+    path = file_name
+    img = cv2.imread(path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+    boxes, weights = hog.detectMultiScale(gray, winStride = (5,30))
+    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+
+    for (xA, yA, xB, yB) in boxes: 
+        # display the detected boxes in the colour picture
+        cv2.rectangle(img, (xA, yA), (xB, yB),
+                            (0, 255, 0), 2)
+
+    return len(boxes)
+
+def count_people_on_img_from_url(url:str) -> int:
+    req = Request(
+    url=url, 
+    headers={'User-Agent': 'Mozilla/5.0'}
+    )
+    webpage = urlopen(req).read()
+    arr = np.asarray(bytearray(webpage), dtype=np.uint8)
+    img = cv2.imdecode(arr, -1) # 'Load it as it is'
+    img = cv2.resize(img, (640, 640))
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+ 
+    boxes, weights = hog.detectMultiScale(gray, winStride = (6,9))
+    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+
+    for (xA, yA, xB, yB) in boxes: 
+        # display the detected boxes in the colour picture
+        cv2.rectangle(img, (xA, yA), (xB, yB),
+                            (0, 255, 0), 2)
+    
+
+    return len(boxes)
+
+def save_images_and_count(files: list[UploadFile] = File(...)):
+    result = []
+    for file in files:
+        file_path = os.path.join("C:\Studia\Lato\Programowanie\Zaawansowane programowanie\ZPP", file.filename)
+        with open(file_path, "wb") as f:
+            shutil.copyfileobj(file.file, f)
+        result.append({file.filename : count_people_on_img(file.filename)})
+    return result
+
---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
+from typing import Annotated
+import logic
+
+app = FastAPI()
+
+@app.get("/from_disc/{file_name}")
+def count_people_on_img(file_name: str):
+    return logic.count_people_on_img(file_name + ".jpg")
+
+@app.get("/files/{url_path:path}")
+def count_people_on_img_url(url_path: str):
+    return logic.count_people_on_img_from_url(url_path)
+
+
+@app.post("/upload_jpg/")
+async def create_upload_files(files: list[UploadFile] = File(...)):
+    result = logic.save_images_and_count(files)
+    return result
+
+@app.get("/")
+async def main():
+    content = """
+<body>
+<form action="/upload_jpg/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</body>
+    """
+    return HTMLResponse(content=content)
